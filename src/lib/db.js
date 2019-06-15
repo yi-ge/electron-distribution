a8be36a3f63ddb1d951ee964df2cd806e7502989
王轶 <a@wyr.me>
2019-06-15 14:00:42 +0800

    No effect

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,7 +4,7 @@ import FileSync from 'lowdb/adapters/FileSync'
 import { SYSTEM } from '../config'
 
 const isDev = process.env.NODE_ENV ? process.env.NODE_ENV === 'development' : false
-const adapter = isDev ? new FileSync(path.join(SYSTEM.NAME + '-distribution-db.json')) : new FileSync(path.join(SYSTEM.WORKPATH, SYSTEM.NAME + '-distribution-db.json'))
+const adapter = new FileSync(isDev ? path.join(SYSTEM.NAME + '-distribution-db.json') : path.join(SYSTEM.WORKPATH, SYSTEM.NAME + '-distribution-db.json'))
 const db = low(adapter)
 
 if (!db.get('appLog').value()) {
